0977944bed14e38287f6083a417b614fb4750290
Xi Wang <xidulu@gmail.com>
2023-12-29 07:01:48 +0000

    remove old files

--- sonar_experiments.py
+++
@@ -1,9 +0,0 @@
-from main import MFVI_with_subsampling_naive
-
-mfvi = MFVI_with_subsampling_naive(
-    model_dir='./models/LogisticRegression',
-    dataset='sonar',
-    observed_vars=['X', 'y']
-)
-
-mfvi.run(num_iters=1000)
